2c2f83b47348e7d75d3a506a802eccd9a7e27ae5
senasoon <senasoon0@gmail.com>
2023-06-08 13:09:28 +0900

    fix: model index 설정 버그 수정 #1

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,17 @@
 import Sequelize from 'sequelize';
 import DEVELOPMENT from '../config/config.js';
-import fs from 'fs';
-import path from 'path';
+import User from './User.js';
 
 const db = {};
 
-export const sequelize = new Sequelize.Sequelize(
+export const sequelize = new Sequelize(
   DEVELOPMENT.database,
   DEVELOPMENT.username,
   DEVELOPMENT.password,
   {
+    database: DEVELOPMENT.database,
+    username: DEVELOPMENT.username,
+    password: DEVELOPMENT.password,
     host: DEVELOPMENT.host,
     dialect: DEVELOPMENT.dialect,
   }
@@ -17,29 +19,8 @@ export const sequelize = new Sequelize.Sequelize(
 
 db.sequelize = sequelize;
 
-const __filename = path.basename(import.meta.url);
-const __dirname = path.dirname(__filename);
+User.initiate(sequelize);
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf('.') !== 0 &&
-      !file.includes('test') &&
-      file !== __filename &&
-      file.slice(-3) === '.js'
-    );
-  })
-  .forEach(async (file) => {
-    // 해당 파일의 모델 불러와서 init
-    const model = await import(path.join(__dirname, file));
-    db[model.name] = model;
-    model.initiate(sequelize);
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+User.associate(db);
 
 export default db;
