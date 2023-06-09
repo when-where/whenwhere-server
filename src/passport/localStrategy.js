f96df3166000ff6a862d7bc7f760c34707d66c56
senasoon <senasoon0@gmail.com>
2023-06-09 19:32:20 +0900

    feat: 로컬 로그인 기능 구현 #5

--- src/passport/localStrategy.js
+++ src/passport/localStrategy.js
@@ -1,1 +1,40 @@
-export default () => {};
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import User from '../models/User';
+import bcrypt from 'bcrypt';
+
+export default () => {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: false,
+      },
+      async (email, password, done) => {
+        try {
+          const exUser = await User.findOne({ where: { email } });
+          if (exUser) {
+            const result = await bcrypt.compare(password, exUser.password);
+            if (result) {
+              done(null, exUser);
+            } else {
+              done(null, false, {
+                code: 'LOGIN_FAILURE',
+                message: '이메일 또는 비밀번호가 일치하지 않습니다.',
+              });
+            }
+          } else {
+            done(null, false, {
+              code: 'LOGIN_FAILURE',
+              message: '이메일 또는 비밀번호가 일치하지 않습니다.',
+            });
+          }
+        } catch (error) {
+          console.error(error);
+          done(error);
+        }
+      }
+    )
+  );
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signUp } from '../controllers/auth.js';
+import { signIn, signUp } from '../controllers/auth.js';
 import { isNotLoggedIn } from '../middlewares/index.js';
 const router = express.Router();
 
 router.post('/signup', isNotLoggedIn, signUp);
+router.post('/signin', isNotLoggedIn, signIn);
 
 export default router;
