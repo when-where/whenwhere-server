cc66e3c51af0ebe38df188e66881645ac8119e27
senasoon <senasoon0@gmail.com>
2023-06-11 23:25:26 +0900

    feat: session 저장을 위한 redis 설정 #5

--- src/app.js
+++ src/app.js
@@ -8,8 +8,21 @@ import passport from 'passport';
 import authRouter from './routes/auth.js';
 import testRouter from './routes/testRoute.js';
 import passportConfig from './passport/index.js';
+import redis from 'redis';
+import RedisStore from 'connect-redis';
 
 dotenv.config();
+
+let redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
+redisClient.connect().catch(console.error);
+
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: 'session:',
+});
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
@@ -39,6 +52,7 @@ app.use(
       httpOnly: true, // 자바스크립트를 통해 세션 쿠키를 사용할 수 없도록
       secure: false, // if true, https 환경에서만 session 정보를 주고 받을 수 있음
     },
+    store: redisStore,
   })
 );
 app.use(passport.initialize());
@@ -47,9 +61,35 @@ passportConfig();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello World!</h1>
+  <h1>Hello World!</h1>
+    <form id="signin-form" action="/auth/signin" method="post">
+      <div class="input-group">
+        <label for="email">이메일</label>
+        <input id="email" type="email" name="email" required autofocus />
+      </div>
+      <div class="input-group">
+        <label for="password">비밀번호</label>
+        <input id="password" type="password" name="password" required />
+      </div>
+      <button id="signin" type="submit" class="btn">로그인</button>
+    </form>
     <a href="/auth/kakao">카카오로 로그인</a>
     <a href="/test">테스트 페이지</a>
+    <form id="signup-form" action="/auth/signup" method="post">
+    <div class="input-group">
+      <label for="signup-email">이메일</label>
+      <input id="signup-email" type="email" name="email" />
+    </div>
+    <div class="input-group">
+      <label for="signup-nickname">닉네임</label>
+      <input id="signup-nickname" type="text" name="nickname" />
+    </div>
+    <div class="input-group">
+      <label for="signup-password">비밀번호</label>
+      <input id="signup-password" type="password" name="password" />
+    </div>
+    <button id="signup-btn" type="submit" class="btn">회원가입</button>
+  </form>
   `);
 });
 
