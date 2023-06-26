6074b4e687e38d745cb72c9a5aa18ada297ab162
senasoon <senasoon0@gmail.com>
2023-06-26 18:58:46 +0900

    feat: 이메일 인증 유무 확인 기능 #17

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -157,3 +157,36 @@ export const verifyUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.query.email } });
+
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        data: null,
+        error: { code: 'VERIFY_FAILURE', message: '사용자를 찾지 못했습니다.' },
+      });
+    }
+
+    if (!user.is_valid) {
+      return res.status(403).send({
+        success: false,
+        data: null,
+        error: { code: 'VERIFY_NOT_COMPLETED', message: '인증이 완료되지 않았습니다.' },
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      data: {
+        status: true,
+      },
+      error: null,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import passport from 'passport';
-import { signIn, signUp, logout, verifyUser } from '../controllers/auth.js';
+import { signIn, signUp, logout, verifyUser, getUserStatus } from '../controllers/auth.js';
 import { isLoggedIn, isNotLoggedIn } from '../middlewares/index.js';
 const router = express.Router();
 
@@ -22,5 +22,6 @@ router.get(
 router.post('/logout', isLoggedIn, logout);
 
 router.get('/confirm/:confirmationCode', verifyUser);
+router.get('/status', getUserStatus);
 
 export default router;
